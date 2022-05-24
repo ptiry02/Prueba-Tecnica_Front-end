64d2e85a10c52262d85f6f266ec702aca0390e07
Paul Tiry <tirypaul@gmail.com>
2022-05-24 22:48:49 +0200

    search and fetch

---
+++ paul-tiry_tecnical-test-app/src/assets/globalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    
+    body{
+        display: flex;
+        justify-content: center;
+        background: ${({ theme }) => theme.colors.lightgray};
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+`
+export default GlobalStyle
